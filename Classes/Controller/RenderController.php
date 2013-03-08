ad1fe3abfb66eaccb1faec6af4b3b8f69a4dc00f
Thomas Allmer <d4kmor@gmail.com>
2013-03-08 17:43:20 +0100

    [FEATURE] more configurable settings

--- Classes/Controller/RenderController.php
+++ Classes/Controller/RenderController.php
@@ -1,4 +1,26 @@
 <?php
+/*                                                                        *
+ * It is free software; you can redistribute it and/or modify it under    *
+ * the terms of the GNU General Public License as published by the Free   *
+ * Software Foundation, either version 3 of the License, or (at your      *
+ * option) any later version.                                             *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General      *
+ * Public License for more details.                                       *
+ *                                                                        *
+ * You should have received a copy of the GNU General Public License      *
+ * along with the script.                                                 *
+ * If not, see http://www.gnu.org/licenses/gpl.html                       *
+ *                                                                        *
+ * The TYPO3 project - inspiring people to share!                         *
+ *                                                                        */
+
+/**
+ *
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License, version 2
+ */
 
 class RenderController {
 
@@ -45,14 +67,17 @@ class RenderController {
 	 */
 	public function generatePdf($html, $settings = array()) {
 		$settings = $this->cleanSettings($settings);
+
 		define('DOMPDF_DIR', str_replace(DIRECTORY_SEPARATOR, '/', realpath(t3lib_extMgm::extPath('dompdf_base') . 'Resources/Private/Php/DomPdf/')));
 		define('DOMPDF_INC_DIR', DOMPDF_DIR . '/include');
 		define('DOMPDF_LIB_DIR', DOMPDF_DIR . '/lib');
 		require_once(DOMPDF_INC_DIR . '/functions.inc.php');
-		define('DOMPDF_FONT_DIR', DOMPDF_DIR . '/lib/fonts/');
-		define('DOMPDF_FONT_CACHE', DOMPDF_FONT_DIR);
+		define('DOMPDF_FONT_DIR', $settings['DOMPDF_FONT_DIR']);
+
+		mkdir(PATH_site . 'typo3temp/dompdf_base/');
+		define('DOMPDF_FONT_CACHE', PATH_site . 'typo3temp/dompdf_base/');
 		define('DOMPDF_TEMP_DIR', sys_get_temp_dir());
-		define('DOMPDF_CHROOT', realpath(DOMPDF_DIR));
+		define('DOMPDF_CHROOT', PATH_site);
 		define('DOMPDF_UNICODE_ENABLED', $settings['DOMPDF_UNICODE_ENABLED']);
 		define('DOMPDF_ENABLE_FONTSUBSETTING', $settings['DOMPDF_ENABLE_FONTSUBSETTING']);
 		define('DOMPDF_PDF_BACKEND', $settings['DOMPDF_PDF_BACKEND']);
@@ -64,8 +89,8 @@ class RenderController {
 		define('DOMPDF_ENABLE_JAVASCRIPT', $settings['DOMPDF_ENABLE_JAVASCRIPT']);
 		define('DOMPDF_ENABLE_REMOTE', $settings['DOMPDF_ENABLE_REMOTE']);
 
-		define('DOMPDF_LOG_OUTPUT_FILE', DOMPDF_FONT_DIR.'log.htm');
-		define('DOMPDF_FONT_HEIGHT_RATIO', 1.1);
+		define('DOMPDF_LOG_OUTPUT_FILE', DOMPDF_FONT_CACHE . 'log.html');
+		define('DOMPDF_FONT_HEIGHT_RATIO', $settings['DOMPDF_FONT_HEIGHT_RATIO']);
 		define('DOMPDF_ENABLE_CSS_FLOAT', $settings['DOMPDF_ENABLE_CSS_FLOAT']);
 		define('DOMPDF_ENABLE_AUTOLOAD', TRUE);
 		define('DOMPDF_AUTOLOAD_PREPEND', FALSE);
@@ -77,8 +102,6 @@ class RenderController {
 			require_once(DOMPDF_INC_DIR . '/autoload.inc.php');
 			require_once(DOMPDF_LIB_DIR . '/php-font-lib/classes/font.cls.php');
 		}
-
-		//mb_internal_encoding('UTF-8');
 
 		global $_dompdf_warnings;
 		$_dompdf_warnings = array();
@@ -92,21 +115,21 @@ class RenderController {
 		global $_DOMPDF_DEBUG_TYPES;
 		$_DOMPDF_DEBUG_TYPES = array(); //array('page-break' => 1);
 
-		define('DEBUGPNG', FALSE);
-		define('DEBUGKEEPTEMP', FALSE);
-		define('DEBUGCSS', FALSE);
+		define('DEBUGPNG', $settings['DEBUGPNG']);
+		define('DEBUGKEEPTEMP', $settings['DEBUGKEEPTEMP']);
+		define('DEBUGCSS', $settings['DEBUGCSS']);
 
-		define('DEBUG_LAYOUT', FALSE);
-		define('DEBUG_LAYOUT_LINES', TRUE);
-		define('DEBUG_LAYOUT_BLOCKS', TRUE);
-		define('DEBUG_LAYOUT_INLINE', TRUE);
-		define('DEBUG_LAYOUT_PADDINGBOX', TRUE);
+		define('DEBUG_LAYOUT', $settings['DEBUG_LAYOUT']);
+		define('DEBUG_LAYOUT_LINES', $settings['DEBUG_LAYOUT_LINES']);
+		define('DEBUG_LAYOUT_BLOCKS', $settings['DEBUG_LAYOUT_BLOCKS']);
+		define('DEBUG_LAYOUT_INLINE', $settings['DEBUG_LAYOUT_INLINE']);
+		define('DEBUG_LAYOUT_PADDINGBOX', $settings['DEBUG_LAYOUT_PADDINGBOX']);
 
 		$dompdf = new DOMPDF();
 		$dompdf->load_html($html);
 
 		$dompdf->render();
-		$dompdf->stream('output.pdf', array(
+		$dompdf->stream($GLOBALS['TSFE']->page['title'], array(
 			'Attachment' => intval($settings['forceDownloadOfPdf'])
 		));
 	}
@@ -118,10 +141,13 @@ class RenderController {
 		$newSettings = array();
 		foreach($settings as $setting => $value) {
 			$value = $value === 'TRUE' || $value === 'true' ? TRUE : $value;
-			$value = $value === 'FALSE' || $value === 'false' ? TRUE : $value;
+			$value = $value === 'FALSE' || $value === 'false' ? FALSE : $value;
 			$newSettings[$setting] = $value;
 		}
 		$newSettings['DOMPDF_DPI'] = intval($settings['DOMPDF_DPI']);
+		$newSettings['DOMPDF_FONT_HEIGHT_RATIO'] = floatval($settings['DOMPDF_FONT_HEIGHT_RATIO']);
+		$newSettings['DOMPDF_FONT_DIR'] = \TYPO3\CMS\Core\Utility\GeneralUtility::getFileAbsFileName($settings['DOMPDF_FONT_DIR']);
+
 		return $newSettings;
 	}
 
